7d652402bc2a27904d3a0477bb6ee0dfe5c34af2
SANHOKIM <awrko@naver.com>
2024-01-25 10:51:03 +0900

    4thdraw arrangement

--- server/web.js
+++ server/web.js
@@ -1,27 +1,29 @@
 const express = require("express");
+const morgan = require("morgan"); //서버 스피드 및 요청체크
 const server = express();
-const adminrouter = require("./adm/");
+const adminrouter = require("./adm/"); //관리자 라우터생성
 
-// 화면노출파일들에게 루트디렉토리설정
+//화면노출파일들에게 루트디렉토리설정
 server.use(express.static(path.join(__dirname, "web")));
-// 주소창에 /admin 요청하면 adm폴더 접근
+//주소창에 /admin을 요청하면 adm폴더 접근
 server.use("/admin", express.static(path.join(__dirname, "adm")));
 server.use("/admin/:id", adminrouter);
 //req.param.변수로 받게됩니다.
 //req.query.변수
 
 //postman으로 서버전송데이터를 확인한다.
-// 나는 get || post 로 오는것만 상대한다. 라우터(관문)생성하는행위?
-server.get("/", (req, res) => {});
-server.post("/from", (req, res) => {});
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "web/index.html"));
+});
+server.post("/form", (req, res) => {});
 
-// 나는 아무거나 괜찮아.. 서버는 요청이 와야 친다..아무거나 요청 오세요..
-
+//제일 마지막
 server.use((req, res, next) => {
-  res.status(404).send("주소똑바로치시오");
-  res.status(404).sendFile(path.join(__dirname, "../web/nopage.html"));
+  //  res.status(404).send("주소똑바로치시오!")
+  res.status(404).sendFile(path.join(__dirname, "web/nopage.html"));
 });
 
+server.use(morgan("dev"));
 server.listen(8001, () => {
   console.log("에러없이구동중");
 });
